2230dbe2b37f266ee35f6a2c81a6209287678cdc
xinggw20 <xinggw20@bu.edu>
2025-09-26 09:51:14 -0400

    sep.26

--- TestCommit.r
+++ TestCommit.r
@@ -1,2 +1,23 @@
 # Test for commit
 # Hello
+set.seed(321)
+
+flips_until_k_heads <- function(p= 0.5, k = 2) {
+  consec <- 0; flips <- 0
+  while(consec < k) {
+    flips <- flips + 1
+    if (runif(1) < p) consec <- consec + 1 else consec <- 0
+  }
+  flips
+}
+
+N <- 10000
+res <- replicate(N, flips_until_k_heads(0.5, 2))
+
+mean(res)
+median(res)
+
+hist(res,
+     breaks = seq(0.5, max(res)+0.5, 1),
+     main = "Full Range",
+     xlab = "flips")
